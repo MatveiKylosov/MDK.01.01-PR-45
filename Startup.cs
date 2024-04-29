57b39728827121c58204fdc4b14a66965734619a
MatveiKylosov <matveikyl@mail.ru>
2024-04-29 18:46:27 +0500

    Настройка проекта, подключение swagger. Kylosov

--- Startup.cs
+++ Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,12 +18,32 @@ namespace API_Kylosov
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddSwaggerGen(c => 
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo 
+                        { 
+                            Version = "v1",
+                            Title = "Руководство для использования запросов",
+                            Description = "Полное руководство для использования запросов находящихся в проекте"
+                        }
+                    );
+
+                    var filtePath = Path.Combine(System.AppContext.BaseDirectory, "API_Kylosov.xml");
+                    c.IncludeXmlComments(filtePath);
+                }
+            );
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseMvcWithDefaultRoute();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Запросы GET"); });
+
+/*            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -34,7 +56,7 @@ namespace API_Kylosov
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
-            });
+            });*/
         }
     }
 }
