8230847fd68f15a8debdfe588db04879eadd8cf8
MatveiKylosov <matveikyl@mail.ru>
2024-04-29 19:14:59 +0500

    Разработанные модели данных. Kylosov

---
+++ Model/Tasks.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace API_Kylosov.Model
+{
+    /// <summary>
+    /// Задачи
+    /// </summary>
+    public class Tasks
+    {
+        /// <summary>
+        /// Код
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Приоритет
+        /// </summary>
+        public string Priority { get; set; }
+        /// <summary>
+        /// Дата выполнения
+        /// </summary>
+        public DateTime DateExecute { get; set; }
+        /// <summary>
+        /// Комментарий
+        /// </summary>
+        public string Comment { get; set; }
+        /// <summary>
+        /// Выполнено
+        /// </summary>
+        public bool Done { get; set; }
+    }
+}
