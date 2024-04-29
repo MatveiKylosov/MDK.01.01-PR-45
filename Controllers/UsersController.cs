755b10b951af3d8ba92712e6d2fa9ccf44fb172f
MatveiKylosov <matveikyl@mail.ru>
2024-04-29 22:47:05 +0500

    Разработанный контроллер авторизации пользователя. Kylosov

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using API_Kylosov.Context;
+using API_Kylosov.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Kylosov.Controllers
+{
+    [Route("api/UsersController")]
+    [ApiExplorerSettings(GroupName = "v2")]
+    public class UsersController : Controller
+    {
+        [Route("SingIn")]
+        [HttpPost]
+        [ProducesResponseType(typeof(Users), 200)]
+        [ProducesResponseType(403)]
+        [ProducesResponseType(500)]
+        public ActionResult SingIn([FromForm] string Login, [FromForm] string Password)
+        {
+            if(Login == null || Password == null)
+                return StatusCode(403);
+            try
+            {
+                Users User = new UsersContext().Users.Where(x => x.Login == Login && x.Password == Password).First();
+                return Json(User);
+            }catch(Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
