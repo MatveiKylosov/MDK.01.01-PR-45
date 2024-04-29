ad834694af1e3ed0d2a5b854baab4c7e85f2a620
MatveiKylosov <150270363+MatveiKylosov@users.noreply.github.com>
2024-04-29 22:33:28 +0500

    Update TaskContoller.cs

--- Controllers/TaskContoller.cs
+++ Controllers/TaskContoller.cs
@@ -11,7 +11,7 @@ namespace API_Kylosov.Controllers
     [Route("api/TasksController")]
     public class TaskContoller : Controller
     {
-/*        [Route("List")]
+        [Route("List")]
         [HttpGet]
         [ProducesResponseType(typeof(List<Tasks>), 200)]
         [ProducesResponseType(500)]
@@ -26,7 +26,7 @@ namespace API_Kylosov.Controllers
             {
                 return StatusCode(500, exp.Message);
             }
-        }*/
+        }
 
         [Route("Item")]
         [HttpGet]
