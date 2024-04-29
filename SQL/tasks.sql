9d6b41ade058c8bbd370706fc71c72eb00c25f54
MatveiKylosov <matveikyl@mail.ru>
2024-04-29 21:58:54 +0500

    sql

---
+++ SQL/tasks.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE `Tasks` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(50) DEFAULT NULL,
+  `Priority` varchar(30) DEFAULT NULL,
+  `DateExecute` date DEFAULT NULL,
+  `Comment` varchar(1000) DEFAULT NULL,
+  `Done` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
