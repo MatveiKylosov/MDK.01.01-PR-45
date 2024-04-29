1c40795adbace98d3f5d458121c934b789c277be
MatveiKylosov <matveikyl@mail.ru>
2024-04-29 19:19:53 +0500

    Разработанный контекст для загрузки данных. Kylosov

---
+++ Context/TasksContext.cs
@@ -0,0 +1,21 @@
+﻿using API_Kylosov.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace API_Kylosov.Context
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=127.0.0.1; uid=root;pwd=;database=TaskManager", new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
