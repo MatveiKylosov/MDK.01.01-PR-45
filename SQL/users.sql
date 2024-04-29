04030165dd4d95af37ed28fdbb51d703954e4839
MatveiKylosov <matveikyl@mail.ru>
2024-04-30 00:57:15 +0500

    fix авторизации

---
+++ SQL/users.sql
@@ -0,0 +1,7 @@
+﻿CREATE DATABASE `users`; /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+CREATE TABLE `Users` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Login` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Password` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
